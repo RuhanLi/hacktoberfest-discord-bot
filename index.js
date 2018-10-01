b396dd9cd80c8b0b3abce9cf5b89b063e44dca62
RuhanLi <talktothehand9@hotmail.com>
2018-10-01 18:27:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ client.on('message', async (msg) => {
     });
   } else if(command === 'scream') {
     msg.reply(args.join(' ').toUpperCase());
+  } else if(command === 'whisper') {
+    msg.reply(args.join(' ').toLowerCase());
   }
 })
 
